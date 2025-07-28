4f419a4b9f195e145ab4a27c910b9bc207ff718a
Deenu Choudhary <deenuchoudhary47@gmail.com>
2025-07-28 15:12:34 +0530

    .

--- Recursion.java
+++ Recursion.java
@@ -6,8 +6,44 @@ public class Recursion {
       System.out.println(num);
       PrintNum(num - 1);
    }
-
+   public static void printSum(int i, int n, int sum){
+      if(i > n){
+         System.out.println(sum);
+         return;
+      }
+      sum = sum + i;
+      printSum(i+1,n,sum);
+   }
+   public static void Factorial(int i, int n, int fact){
+      if(i > n){
+         System.out.println(fact);
+         return;
+      }
+      fact *= i;
+      Factorial(i+1, n, fact);
+   }
+   public static void FibonacciSeries(int n,int a,int b) {
+      if(n == 0)
+         return;
+      System.out.print(a+" , ");
+      FibonacciSeries(n-1, b, a + b);
+   }
+   public static int CalcPower(int x, int n){
+      if(x == 0){
+         return 0;
+      }
+      if(n == 0){
+         return 1;
+      }
+      int ans = x * CalcPower(x, n -1);
+      return ans;
+   }
    public static void main(String[] args) {
-      PrintNum(5);
+      // PrintNum(5);
+      // printSum(1,10,0);
+      // Factorial(1, 5, 1);
+      // FibonacciSeries(10,0,1);
+      // System.out.println(CalcPower(2,5));
+      
    }
 }
